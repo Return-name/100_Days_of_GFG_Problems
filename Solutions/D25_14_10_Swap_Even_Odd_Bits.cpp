fc624ca2e7660bbab5da8a30d9b9df6524844ecd
Return-name <rishabha649@gmail.com>
2021-10-14 07:51:48 +0530

    Day 25 - 14/10/21 Swap all Odd and Even Bits

---
+++ Solutions/D25_14_10_Swap_Even_Odd_Bits.cpp
@@ -0,0 +1,43 @@
+// { Driver Code Starts
+//Initial Template for C++
+
+#include<bits/stdc++.h>
+using namespace std;
+
+
+ // } Driver Code Ends
+//User function Template for C++
+
+class Solution
+{
+    public:
+    //Function to swap odd and even bits.
+    unsigned int swapBits(unsigned int n)
+    {
+    	// Your code here
+    	unsigned int h = 1431655765;
+    	unsigned int ans = 0;
+    	ans += (n>>1)&h;
+    	ans += (n<<1)&(~h);
+    	return ans;
+    }
+};
+
+// { Driver Code Starts.
+
+// Driver code
+int main()
+{
+	int t;
+	cin>>t;//testcases
+	while(t--)
+	{
+		unsigned int n;
+		cin>>n;//input n
+		
+		Solution ob;
+		//calling swapBits() method
+		cout << ob.swapBits(n) << endl;
+	}
+	return 0;
+}  // } Driver Code Ends
