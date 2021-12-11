b609bfe4dd64c5de716ae73fa3677ca2c74bf485
Return-name <rishabha649@gmail.com>
2021-12-11 09:49:54 +0530

    Day 83 - 11/12/21 Possible Words from Phone Digits

---
+++ Solutions/D83_11_12_Possible_Words_From_Phone_Digits.cpp
@@ -0,0 +1,80 @@
+// { Driver Code Starts
+//Initial Template for C++
+
+#include <bits/stdc++.h>
+#include <string>
+
+using namespace std;
+
+
+ // } Driver Code Ends
+//User function Template for C++
+
+class Solution
+{
+    public:
+    vector<char> key[10] = {
+        {},
+        {},
+        {'a', 'b', 'c'},
+        {'d', 'e', 'f'},
+        {'g', 'h', 'i'},
+        {'j', 'k', 'l'},
+        {'m', 'n', 'o'},
+        {'p', 'q', 'r', 's'},
+        {'t', 'u', 'v'},
+        {'w', 'x', 'y', 'z'}
+    };
+    
+    void func(vector<string> &ans, int a[], int N, int p, string cs)
+    {
+        if(p == N)
+        {
+            ans.push_back(cs);
+            return;
+        }
+        for(char c : key[a[p]])
+        {
+            func(ans, a, N, p+1, cs + c);
+        }
+    }
+    
+    //Function to find list of all words possible by pressing given numbers.
+    vector<string> possibleWords(int a[], int N)
+    {
+        //Your code here
+        vector<string> ans;
+        func(ans, a, N, 0, "");
+        return ans;
+    }
+};
+
+
+// { Driver Code Starts.
+
+int main() {
+  
+	int T;
+	
+	cin >> T; //testcases
+	
+	while(T--){ //while testcases exist
+	   int N;
+	    
+	   cin >> N; //input size of array
+	   
+	   int a[N]; //declare the array
+	   
+	   for(int i =0;i<N;i++){
+	       cin >> a[i]; //input the elements of array that are keys to be pressed
+	   }
+	   
+	   Solution obj;
+	   
+	  vector <string> res = obj.possibleWords(a,N);
+	  for (string i : res) cout << i << " ";
+	   cout << endl;
+	}
+	
+	return 0;
+}  // } Driver Code Ends
